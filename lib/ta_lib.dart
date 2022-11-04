3dc4952a6b29900cdb43007b7a1e6007b5a21efc
JosephusZhou <josephuszhou@qq.com>
2022-11-04 15:51:33 +0800

    Feature: export lib class

--- lib/ta_lib.dart
+++ lib/ta_lib.dart
@@ -1,4 +1,12 @@
 library ta_lib;
 
+export 'src/candle_setting.dart';
+export 'src/candle_setting_type.dart';
+export 'src/compatibility.dart';
 export 'src/core.dart';
+export 'src/func_unst_id.dart';
+export 'src/m_integer.dart';
+export 'src/ma_type.dart';
+export 'src/money_flow.dart';
+export 'src/range_type.dart';
 export 'src/ret_code.dart';
